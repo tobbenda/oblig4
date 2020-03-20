0274e2abb8fd64b91e3a68efe923f3bf19c4f5ed
Jorgen693 <jorgenhjem@hotmail.com>
2020-03-20 12:53:34 +0100

    Lagt til kommentarer i Lege.java + endret metoden som returnerer liste.

--- NyeKlasserMappe/Jørgen/Oblig4-master/NyeKlasserMappe/Jørgen/Lege.java
+++ NyeKlasserMappe/Jørgen/Oblig4-master/NyeKlasserMappe/Jørgen/Lege.java
@@ -1,11 +1,13 @@
 class Lege implements Comparable<Lege> {
   protected String navn;
-  private Lenkeliste<Resept> utskrevedeResepter = new Lenkeliste<Resept>();
+  private Lenkeliste<Resept> utskrevedeResepter = new Lenkeliste<Resept>(); //Oppretter en tom Lenkeliste
+                                                                            //som skal inneholde resepter.
 
 
-  public Lege(String navn){
+  public Lege(String navn){  //Konstruktør for lege-klassen
     this.navn = navn;
   }
+
   public String hentNavn(){  //Metode for å hente navn
     return navn;
   }
@@ -14,75 +16,73 @@ class Lege implements Comparable<Lege> {
     return "Navn: " + navn;
   }
 
-  public int compareTo(Lege lege){
+  public int compareTo(Lege lege){  //Metode for å kunne sammenligne legers navn for alfabetisk sortering.
     return navn.compareTo(lege.hentNavn());
   }
 
-  public void hentReseptListe(){
-    for(Resept resept : utskrevedeResepter){
-      System.out.println(resept + "\n");
-    }
+  public Lenkeliste<Resept> hentReseptListe(){   //Metode for å hente ut lista med utskrevederesepter.
+    return utskrevedeResepter;
   }
 
-  public boolean sjekkLege(){
+  public boolean sjekkLege(){  //Metode for å sjekke om vi driver med lege eller spesialist
     if (this instanceof Spesialist){
-      return true;
+      return true;    //True hvis legen er spesialist.
     }
-    return false;
+    return false;     //False om det er en vanlig lege.
   }
 
-  public boolean sjekkLovlighet(Legemiddel legemiddel){
+  public boolean sjekkLovlighet(Legemiddel legemiddel){  //Sjekk for utskrift av narkotisk.
     if(legemiddel instanceof Narkotisk){
       if(this.sjekkLege() == true){
-        return true;
+        return true;   //Kun spesialist får skrive narkotisk
       }else{
-        return false;
+        return false;  //Vanlig lege returnerer false.
       }
     }
-    return true;
+    return true;  //Om stoffet ikke er narkotisk kan alle skrive ut
   }
 
   public HvitResept skrivHvitResept(Legemiddel legemiddel, Pasient pasient, int reit)
-  throws UlovligUtskrift {
+  throws UlovligUtskrift {  //Metode for utskrift av hvite resepter.
     if(sjekkLovlighet(legemiddel) == true){
       HvitResept nyResept = new HvitResept(legemiddel, this, pasient, reit);
-      utskrevedeResepter.leggTil(nyResept);
-      return nyResept;
+      utskrevedeResepter.leggTil(nyResept);  //Legger den nye resepten i utskrevedeResepter-lista
+      return nyResept;  //returnerer den nye resepten.
     }else{
-      throw new UlovligUtskrift(this, legemiddel);
+      throw new UlovligUtskrift(this, legemiddel);  //Om legen ikke har lov å skrive ut kastes exception.
     }
   }
 
   public MilitaerResept skrivMilitaerResept(Legemiddel legemiddel, Pasient pasient, int reit)
-  throws UlovligUtskrift{
+  throws UlovligUtskrift{  //Metode for utskrift av militaer resepter.
     if(sjekkLovlighet(legemiddel) == true){
       MilitaerResept nyResept = new MilitaerResept(legemiddel, this, pasient, reit);
-      utskrevedeResepter.leggTil(nyResept);
-      return nyResept;
+      utskrevedeResepter.leggTil(nyResept);  //Legger den nye resepten i utskrevedeResepter-lista.
+      return nyResept;      //returnere den nye resepten.
     }else{
-      throw new UlovligUtskrift(this, legemiddel);
+      throw new UlovligUtskrift(this, legemiddel);  //Kaster exception om legen ikke er godkjent.
     }
   }
 
   public BlaaResept skrivBlaaResept(Legemiddel legemiddel, Pasient pasient, int reit)
-  throws UlovligUtskrift {
+  throws UlovligUtskrift {  //Metode for utskrift av blaa resepter
     if(sjekkLovlighet(legemiddel) == true){
       BlaaResept nyResept = new BlaaResept(legemiddel, this, pasient, reit);
-      utskrevedeResepter.leggTil(nyResept);
-      return nyResept;
+      utskrevedeResepter.leggTil(nyResept);   //Legger til den nye resepten i utskrevedeResepter-lista.
+      return nyResept;      //Returnerer den nye resepten.
     }else{
-      throw new UlovligUtskrift(this, legemiddel);
+      throw new UlovligUtskrift(this, legemiddel);   //Kaster exception om legen ikke er godkjent.
     }
   }
 
   public PResept skrivPResept(Legemiddel legemiddel, Pasient pasient)
-  throws UlovligUtskrift {
+  throws UlovligUtskrift {  //Metode for utskrift av P-resepter.
     if(sjekkLovlighet(legemiddel) == true){
       PResept nyResept = new PResept(legemiddel, this, pasient);
-      utskrevedeResepter.leggTil(nyResept);
-      return nyResept;
+      utskrevedeResepter.leggTil(nyResept);  //Legger den nye resepten i utskrevedeResepter-lista.
+      return nyResept;  //Returnerer den ny resepten.
     }else{
-      throw new UlovligUtskrift(this, legemiddel);
+      throw new UlovligUtskrift(this, legemiddel);  //Kaster exception om legen ikke er godkjent.
     }
   }
 }
