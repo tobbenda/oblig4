a50ce1eba8537fa695b1473f7cc0e1b242441866
Jorgen693 <jorgenhjem@hotmail.com>
2020-03-21 14:34:19 +0100

    App 0.3 - begynner ta form

--- nyHovedmappe/alleFiler/App.java
+++ nyHovedmappe/alleFiler/App.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.io.FileNotFoundException;
 
 class App{
-  static Legesystem nyttSystem = new Legesystem();
-  public static void main(String[] args) throws InputMismatchException{
+  static Legesystem nyttSystem = new Legesystem();  //Oppretter et legesystem for bruk i appen.
+  public static void main(String[] args) throws InputMismatchException, UlovligUtskrift{
     int menyValg = 0;
-
-    //Legesystem nyttSystem = new Legesystem();
     while(menyValg!=6){                    //Innvalg 6 er Exit, så programmet kjører til 6 blir valgt i hovedmenyen.
       int valg = hovedmeny();              //Metoden hovedmeny ligger under, og tar seg av utskrift til, og input fra, bruker.
       if (valg==1){
@@ -23,33 +22,49 @@ class App{
       menyValg=valg;
     }
   }
-  // private static void filLesing(String filnavn) throws FileNotFoundException{
-  //     nyttSystem.lesFil(filnavn);
-  // }
+
+  private static void filLesing(String filnavn) throws FileNotFoundException, UlovligUtskrift{
+      nyttSystem.lesFil(filnavn);
+  }
 
   private static void skrivUtAlt(){  //Metode for innvalg 1 Skriver ut alle
        System.out.println("Oversikt over alle elementer.");
-       System.out.println("Alle registrerte leger:");
-       for(Lege l : nyttSystem.hentLegeListe()){
-          System.out.println(l);
+       if(nyttSystem.hentLegeListe().stoerrelse() == 0){
+           System.out.println("\nIngen registrerte leger.");
+       }else{
+           System.out.println("\nAlle registrerte leger:");
+           for(Lege l : nyttSystem.hentLegeListe()){
+              System.out.println(l);
+           }
        }
 
-       System.out.println("\nAlle registrerte pasienter:");
-       for(Pasient p : nyttSystem.hentPasientListe()){
-           System.out.println(p);
+       if(nyttSystem.hentPasientListe().stoerrelse() == 0){
+           System.out.println("\nIngen registrerte pasienter.");
+       }else{
+           System.out.println("\nAlle registrerte pasienter:");
+           for(Pasient p : nyttSystem.hentPasientListe()){
+               System.out.println(p);
+           }
        }
-       System.out.println("\nAlle registrerte legemidler:");
-       for(Legemiddel m : nyttSystem.hentLegemiddelListe()){
-           System.out.println(m);
+       if(nyttSystem.hentLegemiddelListe().stoerrelse() == 0){
+           System.out.println("\nIngen registrerte legemidler.");
+       }else{
+           System.out.println("\nAlle registrerte legemidler:");
+           for(Legemiddel m : nyttSystem.hentLegemiddelListe()){
+               System.out.println(m);
+           }
        }
-
-       System.out.println("\nAlle registrerte resepter:");
-       for(Resept r : nyttSystem.hentReseptListe()){
-           System.out.println(r);
+       if(nyttSystem.hentReseptListe().stoerrelse() == 0){
+           System.out.println("\nIngen registrerte resepter.");
+       }else{
+           System.out.println("\nAlle registrerte resepter:");
+           for(Resept r : nyttSystem.hentReseptListe()){
+               System.out.println(r);
+           }
        }
    }
 
-  private static void opprett() throws InputMismatchException{  //Metode for innvalg 2 - DELOPPGAVE E4
+  private static void opprett() throws InputMismatchException, UlovligUtskrift{  //Metode for innvalg 2 - DELOPPGAVE E4
     Scanner oppretterScanner = new Scanner(System.in);
     int oppretteValg = 0;
     while(oppretteValg<1||oppretteValg>4){
@@ -78,34 +93,163 @@ class App{
         opprettNyttLegemiddel();
 
 
-    }else if(oppretteValg==3){; //metode eller kode for å opprette resept
+    }else if(oppretteValg==3){ //Gir bruker valg for å opprette ulike typer resept, HVIS det finnes
+                               //elementer av lege, legemiddel og pasient.
       Scanner typeReseptScanner = new Scanner(System.in);
       int typeReseptValg=0;
-      while(typeReseptValg<1||typeReseptValg>4){    //DELOPPGAVE E7
-        System.out.println("\n Hvilken type resept?");
-        System.out.printf("\n%-17s" + " Tast 1", "Hvit resept?");
-        System.out.printf("\n%-17s" + " Tast 2", "Blå resept?");
-        System.out.printf("\n%-17s" + " Tast 3", "P-resept?");
-        System.out.printf("\n%-17s" + " Tast 4", "Militær-resept?");
-        typeReseptValg = typeReseptScanner.nextInt();
-        if(typeReseptValg<1||typeReseptValg>4){
-          System.out.println("\n\nUgyldig input! Prøv igjen\n");
+      if(nyttSystem.hentLegeListe().stoerrelse() == 0 || nyttSystem.hentLegeListe().stoerrelse() == 0
+        || nyttSystem.hentPasientListe().stoerrelse() == 0){
+            System.out.println("\nKan ikke opprette resept fordi det mangler elementer.");
+        }else{
+            while(typeReseptValg<1||typeReseptValg>4){    //DELOPPGAVE E7
+              System.out.println("\n Hvilken type resept?");
+              System.out.printf("\n%-17s" + " Tast 1", "Hvit resept?");
+              System.out.printf("\n%-17s" + " Tast 2", "Blå resept?");
+              System.out.printf("\n%-17s" + " Tast 3", "P-resept?");
+              System.out.printf("\n%-17s" + " Tast 4\n>", "Militær-resept?");
+              try{
+                  typeReseptValg = typeReseptScanner.nextInt();
+                  if(typeReseptValg<1||typeReseptValg>4){
+                    System.out.println("\n\nUgyldig input! Prøv igjen\n");
+                  }
+              }catch(InputMismatchException e){
+                  System.out.println("\nUgyldig input! Prøv igjen.");
+                  typeReseptScanner.next();
+              }
+            }
         }
+
+
+
+      if(typeReseptValg==1){ //Metode for å opprette hvitresept
+          opprettNyHvitResept();
       }
 
-      //HVIT RESEPT
-      if(typeReseptValg==1){;} //Metode eller kode for å opprette hvit resept
-      //BLÅ RESEPT
-      else if(typeReseptValg==2){;} //Metode eller kode for å opprette Blå resept
-      //P-RESEPT
-      else if(typeReseptValg==3){;} //Metode eller kode for å opprette P-resept
-      //MIL-RESEPT
-      else if(typeReseptValg==4){;} //Metode eller kode for å opprette Militær-resept
-    //PASIENT
-    }else if(oppretteValg==4){;}  //metode eller kode for å opprette pasient
+      else if(typeReseptValg==2){ //Metode for å opprette Blåresept
+          opprettNyBlaaResept();
+      }
 
-  } // Slutten på metoden for innvalget "OPPRETT"
+      else if(typeReseptValg==3){ //Metode for å opprette P-resept
+          opprettNyPResept();
+      }
 
+      else if(typeReseptValg==4){//Metode for å opprette Militær-resept
+          opprettNyMilitaerResept();
+      }
+
+
+  }else if(oppretteValg==4){  //Metode for å opprette pasient
+        opprettNyPasient();
+    }
+
+} // Slutten på metoden Opprett
+
+
+
+
+
+  private static void opprettNyPasient(){
+      System.out.print("\nVennligst oppgi navnet på pasienten du vil opprette.\n>");
+      String pasientNavn = navnSjekk();
+      System.out.print("\nVennligst oppgi fødselsnummeret på pasienten.\n>");
+      String pasientFnr = fnrSjekk();
+      Pasient nyPasient = new Pasient(pasientNavn, pasientFnr);
+      nyttSystem.hentPasientListe().leggTil(nyPasient);
+  }
+
+  private static void opprettNyHvitResept() throws UlovligUtskrift{  //Metode for å opprette hvitresept.
+      Lege aktuellLege = hentAktuellLege();
+      Legemiddel aktueltLegemiddel = hentAktueltLegemiddel();
+      Pasient aktuellPasient = hentAktuellPasient();
+      System.out.print("\nVennligst oppgi antall reit på resepten\n>");
+      int aktuellReit = intSjekk();
+      aktuellLege.skrivHvitResept(aktueltLegemiddel, aktuellPasient, aktuellReit);
+  }
+
+  private static void opprettNyBlaaResept() throws UlovligUtskrift{  //Metode for å opprette blaaresept.
+      Lege aktuellLege = hentAktuellLege();
+      Legemiddel aktueltLegemiddel = hentAktueltLegemiddel();
+      Pasient aktuellPasient = hentAktuellPasient();
+      System.out.print("\nVennligst oppgi antall reit på resepten\n>");
+      int aktuellReit = intSjekk();
+      aktuellLege.skrivBlaaResept(aktueltLegemiddel, aktuellPasient, aktuellReit);
+  }
+
+  private static void opprettNyMilitaerResept() throws UlovligUtskrift{  //Metode for å opprette militaerresept.
+      Lege aktuellLege = hentAktuellLege();
+      Legemiddel aktueltLegemiddel = hentAktueltLegemiddel();
+      Pasient aktuellPasient = hentAktuellPasient();
+      System.out.print("\nVennligst oppgi antall reit på resepten\n>");
+      int aktuellReit = intSjekk();
+      aktuellLege.skrivMilitaerResept(aktueltLegemiddel, aktuellPasient, aktuellReit);
+  }
+
+  private static void opprettNyPResept() throws UlovligUtskrift {  //Metode for å opprette p-resept.
+      Lege aktuellLege = hentAktuellLege();
+      Legemiddel aktueltLegemiddel = hentAktueltLegemiddel();
+      Pasient aktuellPasient = hentAktuellPasient();
+      aktuellLege.skrivPResept(aktueltLegemiddel, aktuellPasient);
+  }
+
+  private static Lege hentAktuellLege() throws UlovligUtskrift{  //Metode for å hente lege til reseptutskrift.
+      System.out.print("\nVennligst oppgi navnet på legen som skal opprette resepten\n>");
+      String valgtLege = navnSjekk();
+      Lege aktuellLege = nyttSystem.hentLegeListe().hent(0);
+      boolean finsLegen = false;
+      while (!finsLegen){
+          for(Lege lege : nyttSystem.hentLegeListe()){
+              if(lege.hentNavn().equals(valgtLege)){
+                  finsLegen = true;
+                  aktuellLege = lege;
+              }
+          }
+          if(!finsLegen){
+              System.out.print("\nLegenavnet du oppga er ikke registrert. Vennligst prøv igjen.\n>");
+              valgtLege = navnSjekk();
+          }
+      }
+      return aktuellLege;
+  }
+
+  private static Legemiddel hentAktueltLegemiddel() throws UlovligUtskrift{  //Metode for å hente legemiddelet til reseptutskrift.
+      System.out.print("\nVennligst oppgi navnet på legemiddelet som skal på resepten\n>");
+      String valgtLegemiddel = navnSjekk();
+      Legemiddel aktueltLegemiddel = nyttSystem.hentLegemiddelListe().hent(0);
+      boolean finsMiddelet = false;
+      while (!finsMiddelet){
+          for(Legemiddel legemiddel : nyttSystem.hentLegemiddelListe()){
+              if(legemiddel.hentNavn().equals(valgtLegemiddel)){
+                  finsMiddelet = true;
+                  aktueltLegemiddel = legemiddel;
+              }
+          }
+          if(!finsMiddelet){
+              System.out.print("\nLegemiddelet du oppga er ikke registrert. Vennligst prøv igjen.\n>");
+              valgtLegemiddel = navnSjekk();
+          }
+      }
+      return aktueltLegemiddel;
+  }
+
+  private static Pasient hentAktuellPasient() throws UlovligUtskrift{  //Metode for å hente pasient til reseptutskrift.
+      System.out.print("\nVennligst oppgi id på pasienten som skal motta resepten\n>");
+      int valgtPasient = intSjekk();
+      Pasient aktuellPasient = nyttSystem.hentPasientListe().hent(0);
+      boolean finsPasienten = false;
+      while (!finsPasienten){
+          for(Pasient pasient : nyttSystem.hentPasientListe()){
+              if(pasient.hentId() == valgtPasient){
+                  finsPasienten = true;
+                  aktuellPasient = pasient;
+              }
+          }
+          if(!finsPasienten){
+              System.out.print("\nPasienten du oppga er ikke registrert. Vennligst prøv igjen.\n>");
+              valgtPasient = intSjekk();
+          }
+      }
+      return aktuellPasient;
+  }
 
   // METODE FOR Å BRUKE RESEPT:
   private static void brukResept(){;} //Metode for innvalg 3 - DELOPPGAVE E5
@@ -222,13 +366,29 @@ class App{
       while(!sjekk){
           try{
               int ikkeFunk = Integer.parseInt(navn);  //Får vi tall som input gir vi feilmelding.
-              System.out.print("\nUgyldig input! Prøv igjen.\n>");
+              System.out.print("\nUgyldig input! Forventet er et svar bestående av tekst.\n>");
               navn = nyttNavnScanner.next();
           }catch(Exception e){
               sjekk = true;
           }
       }
       return navn;   //Returnerer det lovlige navnet
+  }
+
+  private static String fnrSjekk(){  //Metode for å sjekke brukers input hvor det blir spurt om navn/tekst.
+      Scanner nyttFnrScanner = new Scanner(System.in);
+      String fnr = nyttFnrScanner.next();
+      boolean sjekk = false;
+      while(!sjekk){
+          try{
+              int funk = Integer.parseInt(fnr);  //Får vi tall som input er alt som det skal!
+              sjekk = true;
+          }catch(Exception e){
+              System.out.print("\nUgyldig input! Forventet er et svar bestående av tall.\n>");
+              fnr = nyttFnrScanner.next();
+          }
+      }
+      return fnr;   //Returnerer det lovlige fødselsnummeret
   }
 
   private static double doubleSjekk(){  //Metode for å sjekke brukers double-inputs.
@@ -241,7 +401,7 @@ class App{
               doubleTall = nyDouble;
               sjekk = false;
           }catch(InputMismatchException e){
-              System.out.print("\nUgyldig input! Prøv igjen.\n>");
+              System.out.print("\nUgyldig input! Forventet er et svar bestående av et tall.\n>");
               doubleScanner.next();  //Pusher scanner til å be om ny input.
               }
       }
@@ -258,7 +418,7 @@ class App{
               intTall = nyttTall;
               sjekk = false;
           }catch(InputMismatchException e){
-              System.out.print("\nUgyldig input! Prøv igjen.\n>");
+              System.out.print("\nUgyldig input! Forventet er et svar bestående av et heltall.\n>");
               intScanner.next();  //Pusher scanner til å be om ny input.
               }
       }
